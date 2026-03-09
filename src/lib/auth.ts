a3330e681fa3771cea2ba3941aad078de31c695b
GabrielSakoda <otanigabriel0@gmail.com>
2026-03-09 15:02:40 -0300

    chore: add scalar docs setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { openAPI } from "better-auth/plugins";
 
 import { PrismaClient } from "../generated/prisma/client.js";
 
@@ -9,11 +10,12 @@ const prisma = new PrismaClient({
 });
 
 export const auth = betterAuth({
-    trustedOrigins: ["http://localhost:3000"],
+  trustedOrigins: ["http://localhost:3000"],
   emailAndPassword: {
     enabled: true,
   },
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  plugins: [openAPI()],
 });
